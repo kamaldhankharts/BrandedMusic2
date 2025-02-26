38b5b3cd47ea9ad419b030179627a1c24a2fb579
kamaldhankharts <kamaldhankhararts@gmail.com>
2025-02-26 21:37:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,13 +89,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/75bkxk.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/mo2u0v.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/75bkxk.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/mo2u0v.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/75bkxk.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/mo2u0v.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/ebc4dc6357be06e08a3ed.jpg"
