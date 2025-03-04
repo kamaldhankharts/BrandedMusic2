4dd54625c41224cf35502d85ebbc96ed93b21841
kamaldhankharts <kamaldhankhararts@gmail.com>
2025-03-04 18:55:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,10 +89,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/mo2u0v.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/qy5ebp.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/mo2u0v.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/qy5ebp.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/mo2u0v.jpg"
