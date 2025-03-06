dc8b0734ddd259501081abbf086688dc66212f88
kamaldhankharts <kamaldhankhararts@gmail.com>
2025-03-06 13:25:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/kamaldhankharts/BrandedMusic",
+    "https://github.com/kamaldhankharts/BrandedMusic2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
